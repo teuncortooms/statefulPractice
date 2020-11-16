5b5195e9569c71868c30d96057d6f79d0e8d5079
Teun Cortooms <teuncortooms@gmail.com>
2020-11-16 15:23:16 +0100

    first practice

---
+++ chatServer/src/main/java/StatefulPractice/chat/Message.java
@@ -0,0 +1,15 @@
+package StatefulPractice.chat;
+
+public class Message {
+    private String from;
+    private String to;
+    private String content;
+
+    public void setFrom(String s) {
+    }
+
+    public void setContent(String s) {
+    }
+
+    //standard constructors, getters, setters
+}

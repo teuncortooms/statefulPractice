b26b60e10ed1dadafaf1738b8815608058f2c9f9
Teun Cortooms <teuncortooms@gmail.com>
2020-11-18 22:07:26 +0100

    Add EasyNetQTest

---
+++ EasyNetQTest/Publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main()
+        {
+            using var bus = RabbitHutch.CreateBus("host=localhost");
+            var input = "";
+            Console.WriteLine("Enter a message. 'Quit' to quit.");
+            while ((input = Console.ReadLine()) != "Quit")
+            {
+                bus.PubSub.Publish(new TextMessage
+                {
+                    Text = input
+                });
+            }
+        }
+    }
+}
